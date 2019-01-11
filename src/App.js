fe11ae1afc25675726b8ec6c0cf7438723b34842
Darren SooHoo <dsoohoo@MossBeachHomes.com>
2019-01-11 12:28:47 -0800

    Checkpoint 2764 List Chat Rooms: Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAxgubQeHtmBk_uEIoqRJVsP08Rd4ffOuo",
+    authDomain: "bloc-chat-2d269.firebaseapp.com",
+    databaseURL: "https://bloc-chat-2d269.firebaseio.com",
+    projectId: "bloc-chat-2d269",
+    storageBucket: "bloc-chat-2d269.appspot.com",
+    messagingSenderId: "565757088269"
+  };
+  firebase.initializeApp(config);
+ 
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
         </header>
+        <main>
+        <RoomList firebase={firebase} />
+        </main>
       </div>
     );
   }
